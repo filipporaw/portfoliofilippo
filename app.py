14d8b3c38501114bb7b21474b419e0412c2b457e
filipporaw <116605816+filipporaw@users.noreply.github.com>
2025-03-27 12:20:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ FONT_PATH = "Mayonice.ttf"
 CSV_PATH = "partecipanti.csv"
 TEMPLATE_BASENAME = "ricetta_ssn"
 TEMPLATE_OPTIONS = [f"{TEMPLATE_BASENAME}{i}.png" for i in range(1, 8)]
-POS_NOME = (215, 320)
-FONT_SIZE = 36
+POS_NOME = (215, 335)
+FONT_SIZE = 40
 
 # Carica font
 try:
